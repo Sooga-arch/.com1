811f33af10721f274f04d40828d0cf8221627202
Sooga-arch <kulikowskiigor099@gmail.com>
2026-02-02 21:03:27 +0100

    Update script.js

--- Mapa/Mapa/script.js
+++ Mapa/Mapa/script.js
@@ -1,9 +1,25 @@
-html, body {
-    margin: 0;
-    height: 100%;
-}
+const MAP_SIZE = 8192;
+const bounds = [[0, 0], [MAP_SIZE, MAP_SIZE]];
 
-#map {
-    width: 100%;
-    height: 100%;
-}
+const map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -4,
+    maxZoom: 2,
+    zoomControl: true,
+    dragging: true,
+    scrollWheelZoom: true,
+    touchZoom: true
+});
+
+L.imageOverlay(
+    "https://i.imgur.com/6Z8YB7m.jpg", // obraz mapy GTA V
+    bounds
+).addTo(map);
+
+map.fitBounds(bounds);
+map.setView([MAP_SIZE / 2, MAP_SIZE / 2], -2);
+
+map.setMaxBounds(bounds);
+map.on("drag", () => {
+    map.panInsideBounds(bounds, { animate: false });
+});
